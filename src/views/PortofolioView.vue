6c95763adf420b7d44b2a9aafb58023a10c838be
= <adrianagustinust@gmail.com>
2023-11-14 14:25:46 +0700

    add link to image, add project

--- src/views/PortofolioView.vue
+++ src/views/PortofolioView.vue
@@ -8,16 +8,25 @@ export default {
         title: 'wayland desktop',
         img: 'https://raw.githubusercontent.com/AAdrian565/projects/main/project_wayland.png',
         description: 'mainly built by using waybar (css) and eww (scss, yuck)',
+        link: 'https://github.com/AAdrian565/dotfiles'
       },
       {
         title: 'BookIt',
         img: 'https://raw.githubusercontent.com/AAdrian565/projects/main/project_bookit.png',
         description: 'Room booking app built using laravel',
+        link: 'https://github.com/AAdrian565/BOOKit'
+      },
+      {
+        title: 'Event Radar',
+        img: 'https://raw.githubusercontent.com/AAdrian565/projects/main/project_eventradar.png',
+        description: 'Event searching web created using html, css (bootstrap 5), javascript',
+        link: 'https://github.com/deltathrs/EventRadar'
       },
       {
         title: 'Library',
         img: 'https://raw.githubusercontent.com/AAdrian565/projects/main/project_library.png',
         description: 'Library boooking app build in java',
+        link: 'https://github.com/adoniagracio/Library_Java'
       },
     ];
 
@@ -32,13 +41,15 @@ export default {
   <div class="content">
     <ul>
       <li v-for="(item, index) in items" :key="index" class="item-list">
-        <div class="item-wrapper">
-          <img :src="item.img" alt="item.title" class="hover-image" />
-          <div class="item-details">
-            <h1>{{ item.title }}</h1>
-            <p>{{ item.description }}</p>
+        <a :href="item.link">
+          <div class="item-wrapper">
+            <img :src="item.img" alt="item.title" class="hover-image" />
+            <div class="item-details">
+              <h1>{{ item.title }}</h1>
+              <p>{{ item.description }}</p>
+            </div>
           </div>
-        </div>
+        </a>
       </li>
     </ul>
   </div>
@@ -76,11 +87,11 @@ ul {
   box-sizing: border-box;
   z-index: 1;
   opacity: 0;
-  transition: opacity 0.5s; 
+  transition: opacity 0.5s;
 }
 
 .item-wrapper:hover .item-details {
-  opacity: 1; 
+  opacity: 1;
 }
 
 .item-details::before {
@@ -90,13 +101,13 @@ ul {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.8); 
+  background-color: rgba(0, 0, 0, 0.8);
   z-index: -1;
 }
 
 .item-details h1,
 .item-details p {
   margin: 0;
-  color: white; 
+  color: white;
 }
 </style>
